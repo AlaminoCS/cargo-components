53d01c854c50fd35105e02f72c28e633e804e375
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-03-13 23:01:04 -0300

    context nav

--- src/01-ui/components/03-organisms/TopNavOrg.tsx
+++ src/01-ui/components/03-organisms/TopNavOrg.tsx
@@ -1,58 +1,17 @@
-import ContactNavMolecule from '../02-molecules/ContactNavMolecule'
-import MainNavMolecule from '../02-molecules/MainNavMolecule'
-import EmailIcon from '@mui/icons-material/Email'
-import PhoneIcon from '@mui/icons-material/Phone'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
+import React from 'react';
+import ContactNavMolecule from '../02-molecules/ContactNavMolecule';
+import MainNavMolecule from '../02-molecules/MainNavMolecule';
+import { useTopNavContext } from '../../../05-contexts/TopNavContext';
 
-const TopNavOrg = () => {
-
-  const IMAGES = {
-      logo : new URL('../../../04-assets/images/logo.png', import.meta.url).href
-  }
-
-  const contactLinks = [
-    {
-      text: 'contato@pizzaparty.com',
-      href: 'mailto:contato@pizzaparty.com',
-      icon: <EmailIcon />,
-    },
-    {
-      text: '+55 11 99999-9999',
-      href: 'tel:+5511999999999',
-      icon: <PhoneIcon />,
-    },
-    {
-      text: 'Rua das Pizzas, 456',
-      href: 'https://maps.google.com/?q=R.+das+Pizzas,+456',
-      icon: <LocationOnIcon />,
-    },
-  ];
-
-  const mainLinks = [
-    {
-      text: 'Quem somos',
-      href: '#quem-somos',
-    },
-    {
-      text: 'Nossos diferenciais',
-      href: '#nossos-diferenciais',
-    },
-    {
-      text: 'Serviços',
-      href: '#servicos',
-    },
-    {
-      text: 'Contato',
-      href: '#contato',
-    },
-  ];
+const TopNavOrg: React.FC = () => {  
+  const { contactLinks, mainLinks, logoSrc } = useTopNavContext();
 
   return (
     <div>
       <ContactNavMolecule links={contactLinks} />
-      <MainNavMolecule links={mainLinks} logoSrc={IMAGES.logo} />
+      <MainNavMolecule links={mainLinks} logoSrc={logoSrc} />
     </div>
-  )
-}
+  );
+};
 
-export default TopNavOrg
+export default TopNavOrg;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+// src/index.tsx
 export { default as Button } from './Button';
 export { lightTheme, darkTheme } from './theme';
 export { default as GlobalStyles } from './styles/GlobalStyles';
 export { default as ContactNavMolecule } from './01-ui/components/02-molecules/ContactNavMolecule';
 export { default as MainNavMolecule } from './01-ui/components/02-molecules/MainNavMolecule';
+export { default as TopNavOrg } from './01-ui/components/03-organisms/TopNavOrg';
+export { TopNavProvider } from './05-contexts/TopNavContext';
