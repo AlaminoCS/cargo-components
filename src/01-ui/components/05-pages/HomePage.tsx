27ef418b27499600ac7cdd7316edf6d851ee8cfa
Vanderlei José Alamino <valamino@mtrix.com.br>
2025-04-03 19:06:42 -0300

    correção de homePage

--- src/01-ui/components/05-pages/HomePage.tsx
+++ src/01-ui/components/05-pages/HomePage.tsx
@@ -1,32 +1,11 @@
 // src/components/05-pages/HomePage.tsx
 import React from 'react';
-
-import { differentialData } from '../../../07-mocks/differentialsData';
-import { footerData } from '../../../07-mocks/footerData';
-import { headerData } from '../../../07-mocks/headerData';
-import { locationData } from '../../../07-mocks/locationData';
-import { sectionWithImagesData } from '../../../07-mocks/sectionWithImagesData';
-import { separatorImageData } from '../../../07-mocks/separatorImageData';
-import { sectionData } from '../../../07-mocks/servicesData';
-import { socialMediaLinks } from '../../../07-mocks/socialMediaData';
-import { whatsappData } from '../../../07-mocks/whatsappData';
-import { whoWeAreData } from '../../../07-mocks/whoWeAreData';
-import WhatsAppButtonAtom from '../01-atoms/WhatsAppButtonAtom';
 import SectionOurServicesOrg from '../03-organisms/SectionOurServicesOrg';
-import ContactForm from '../04-templates/ContactForm';
-import Footer from '../04-templates/Footer';
 import HeaderTpl from '../04-templates/HeaderTpl';
 import SectionContainerImageTpl from '../04-templates/SectionContainerImageTpl';
 import SectionDifferentialsTpl from '../04-templates/SectionDifferentialsTpl';
 import SectionLocationTpl from '../04-templates/SectionLocationTpl';
 import SectionWhoWeAreTpl from '../04-templates/SectionWhoWeAreTpl';
-<<<<<<< HEAD
-import SectionWithImages from '../04-templates/SectionWithImages';
-
-// Importando dados mockados
-
-const HomePage = () => {
-=======
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -344,28 +323,17 @@ const HomePage = () => {
   };
   
 
->>>>>>> feature/lgpd-policies
   return (
     <>
-      <HeaderTpl
-        {...headerData}
-        iconName="Email"
-        contactLinks={headerData.contactLinks.map((link, index) => ({
-          ...link,
-          iconName: index === 0 ? 'Email' : index === 1 ? 'Phone' : 'LocationOn',
-        }))}
-      />
+      <HeaderTpl {...headerData} />
       <SectionOurServicesOrg {...sectionData} />
       <SectionWhoWeAreTpl {...whoWeAreData} />
-      <SectionContainerImageTpl image={separatorImageData} />
+      <SectionContainerImageTpl image={imageData} />
       <SectionDifferentialsTpl {...differentialData} />
       <SectionLocationTpl {...locationData} />
       <ContactForm phoneNumber={whatsappData.phoneNumber} />
       <SectionWithImages {...sectionWithImagesData} />
-<<<<<<< HEAD
-=======
       <WhatsAppButtonAtom {...whatsappData} />
->>>>>>> feature/lgpd-policies
       <Footer
         socialMedia={socialMediaLinks.map(link => ({
           ...link,
@@ -373,10 +341,6 @@ const HomePage = () => {
         }))}
         {...footerData}
       />
-<<<<<<< HEAD
-      <WhatsAppButtonAtom {...whatsappData} />
-=======
->>>>>>> feature/lgpd-policies
     </>
   );
 };
